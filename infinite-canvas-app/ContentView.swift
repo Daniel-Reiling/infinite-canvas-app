2399438e9c9533c8a01ef53b3f262be9bca57ca4
Daniel Reiling <daniel.reiling@gmail.com>
2022-03-31 09:38:45 -0500

    Initial Commit

---
+++ infinite-canvas-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  infinite-canvas-app
+//
+//  Created by Daniel Reiling on 3/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ infinite-canvas-app/infinite_canvas_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  infinite_canvas_appApp.swift
+//  infinite-canvas-app
+//
+//  Created by Daniel Reiling on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct infinite_canvas_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
